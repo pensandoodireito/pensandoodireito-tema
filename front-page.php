30684657a1e1d0f3b5d7c4d8a68f3f95fbb2ffc7
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-03-20 12:23:33 -0300

    verificação de post antes de chamada do loop

--- front-page.php
+++ front-page.php
@@ -88,50 +88,54 @@
     $fp_pub_query = new WP_Query(array (
         'post_type' => 'publicacao',
         'posts_per_page' => 1));
-    $fp_pub_query->the_post();?>
 
-    <div class="container">
-            <div class="row mt-md" id="publicacoes">
-                <div class="col-md-8" id="publicacao-destaque">
-                        <h3 class="font-roboto red">Publicações da Série Pensando o Direito</h3>
-                      <div class="panel-body">
-                        <div class="col-xs-6 col-md-4">
-                            <a href="#" class="nounderline">
-                              <div class="destaque text-center"> 
-                               <p><?php the_title(); ?></p>
-                              </div>
-                            </a>
-                        </div>
-                        <div class="description col-md-8">
-                            <h4 class="font-roboto red">Volume <?php echo get_post_meta(get_the_ID(), 'pub_number', true); ?></h4>
-                            <p><mark>Data da publicação: <?php echo get_post_meta(get_the_ID(), 'pub_date', true); ?></mark></p>
-                            <p><?php the_excerpt(); ?> <a href="<?php echo get_post_permalink(); ?>">Leia mais</a></p>
-                            <div class="row">
-                             <div class="col-md-12 text-left">
-                                 <div class="btn-group mt-sm" role="group">
-                                     <?php
-                                     $dld_file = get_post_meta(get_the_ID(), 'pub_dld_file', true);
-                                     if( ! empty($dld_file)) { ?>
-                                         <a href="<?php echo get_post_meta(get_the_ID(), 'pub_dld_file', true); ?>" class="btn btn-default"><span class="fa fa-download"></span> BAIXAR</a>
-                                     <?php } else { ?>
-                                         <a href="<?php echo get_post_permalink(); ?>" class="btn btn-default"><span class="fa fa-download"></span> BAIXAR</a>
-                                     <?php }?>
+    if ($fp_pub_query->have_posts()) {
+
+
+        $fp_pub_query->the_post();?>
+
+        <div class="container">
+                <div class="row mt-md" id="publicacoes">
+                    <div class="col-md-8" id="publicacao-destaque">
+                            <h3 class="font-roboto red">Publicações da Série Pensando o Direito</h3>
+                          <div class="panel-body">
+                            <div class="col-xs-6 col-md-4">
+                                <a href="#" class="nounderline">
+                                  <div class="destaque text-center">
+                                   <p><?php the_title(); ?></p>
+                                  </div>
+                                </a>
+                            </div>
+                            <div class="description col-md-8">
+                                <h4 class="font-roboto red">Volume <?php echo get_post_meta(get_the_ID(), 'pub_number', true); ?></h4>
+                                <p><mark>Data da publicação: <?php echo get_post_meta(get_the_ID(), 'pub_date', true); ?></mark></p>
+                                <p><?php the_excerpt(); ?> <a href="<?php echo get_post_permalink(); ?>">Leia mais</a></p>
+                                <div class="row">
+                                 <div class="col-md-12 text-left">
+                                     <div class="btn-group mt-sm" role="group">
+                                         <?php
+                                         $dld_file = get_post_meta(get_the_ID(), 'pub_dld_file', true);
+                                         if( ! empty($dld_file)) { ?>
+                                             <a href="<?php echo get_post_meta(get_the_ID(), 'pub_dld_file', true); ?>" class="btn btn-default"><span class="fa fa-download"></span> BAIXAR</a>
+                                         <?php } else { ?>
+                                             <a href="<?php echo get_post_permalink(); ?>" class="btn btn-default"><span class="fa fa-download"></span> BAIXAR</a>
+                                         <?php }?>
+                                     </div>
+                                     <div class="btn-group btn-danger mt-sm" role="group">
+                                         <a href="<?php echo get_post_permalink(); ?>" class="btn btn-default btn-danger">VISUALIZAR</a>
+                                     </div>
                                  </div>
-                                 <div class="btn-group btn-danger mt-sm" role="group">
-                                     <a href="<?php echo get_post_permalink(); ?>" class="btn btn-default btn-danger">VISUALIZAR</a>
-                                 </div>
-                             </div>
-                           </div>   
+                               </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4 well box-publicacao">
-                    <h5 class="red">Saiba mais sobre a série Pensando o Direito</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet cursus metus, nec feugiat sablandit diam facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet</p>
-                    <strong><a href="publicacao">Todas as publicações</a></strong></br>
+                    <div class="col-md-4 well box-publicacao">
+                        <h5 class="red">Saiba mais sobre a série Pensando o Direito</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet cursus metus, nec feugiat sablandit diam facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet</p>
+                        <strong><a href="publicacao">Todas as publicações</a></strong></br>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-<?php get_footer(); ?>
+<?php }
+get_footer(); ?>
