67ca3a4529666843e1c53cc7ebbf441857822ac6
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-03-23 00:49:13 -0300

    ajuste no link todas as publicações para chamar o archive correto

--- front-page.php
+++ front-page.php
@@ -72,7 +72,7 @@
                     <div class="col-md-4 well box-publicacao">
                         <h5 class="red">Saiba mais sobre a série Pensando o Direito</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet cursus metus, nec feugiat sablandit diam facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet</p>
-                        <strong><a href="publicacao">Todas as publicações</a></strong></br>
+                        <strong><a href="<?php echo get_post_type_archive_link('publicacao'); ?>">Todas as publicações</a></strong></br>
                     </div>
                 </div>
             </div>
