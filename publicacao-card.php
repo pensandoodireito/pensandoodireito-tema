fefdd8b889788016269eb5904bf4ba6201cbfec7
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-04-14 17:53:46 -0300

    Fix pensandoodireito/participacao-sitebase#22 Compartilhamento Social nas Publicações. Exceto FACEBOOK

--- publicacao-card.php
+++ publicacao-card.php
@@ -35,15 +35,15 @@ $autores = false;
       <?php } ?>
       <div id="social-bar">
         <small>
-          <a href="#" class="nounderline">
+          <!--a href="#" class="nounderline"-->
             <span class="fa-stack fa-lg">
               <i class="fa fa-square-o fa-stack-2x"></i>
               <i class="fa fa-facebook fa-stack-1x"></i>
             </span>
-          </a>
+          <!--/a-->
         </small>
         <small>
-          <a href="#" class="nounderline">
+          <a href="#" onClick="event.preventDefault(); window.open('https://twitter.com/share?hashtags=pensandoodireito&text=<?php echo the_title(); ?>&url=<?php echo get_post_permalink(); ?>','Tweet');" class="nounderline">
             <span class="fa-stack fa-lg">
               <i class="fa fa-square-o fa-stack-2x"></i>
               <i class="fa fa-twitter fa-stack-1x"></i>
@@ -51,7 +51,7 @@ $autores = false;
           </a>
         </small>
         <small>
-          <a href="#" class="nounderline">
+          <a href="#" onClick="event.preventDefault(); window.open('https://www.linkedin.com/shareArticle?mini=true&title=<?php echo urlencode(get_the_title()); ?>&url=<?php echo urlencode(get_post_permalink()); ?>&summary=<?php echo urlencode(get_the_excerpt()); ?>&source=http://participacao.mj.gov.br');" class="nounderline">
             <span class="fa-stack fa-lg">
               <i class="fa fa-square-o fa-stack-2x"></i>
               <i class="fa fa-linkedin fa-stack-1x"></i>
