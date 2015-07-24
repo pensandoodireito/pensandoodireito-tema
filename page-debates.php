94e644b9612c046a71a8e4b3dd81db17b1851ce3
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-07-24 11:56:14 -0300

    Montar a página do custom post type debates

--- page-debates.php
+++ page-debates.php
@@ -1,47 +1,196 @@
 <?php get_header(); ?>
-        <div class="container">
-            <div class="row mt-md" id="debates">
-                <div class="col-md-6">
-                    <div class="debate-item protecao">
-                        <div class="text-center">
-                    <a href="<?php echo site_url("/dadospessoais"); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/protecao-w.png" class="img-adptive" alt="Proteção de Dados Pessoais"></a>
-                </div>
-                <div class="description">
-                        <?php
-                            $page_dadospessoais = get_page_by_title('Proteção de Dados Pessoais');
-
-                            if ($page_dadospessoais != null) {
-                                echo $page_dadospessoais->post_content;
-                            }
-                        ?>
-                </div>
-            </div>
-            <div class="mt-sm text-center">
-                <a href="<?php echo site_url("/dadospessoais"); ?>" class="btn btn-danger font-roboto">Participe deste debate!</a>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="debate-item marco-civil">
-                <div class="text-center">
-                    <a href="<?php echo site_url("/marcocivil"); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/marcocivil-b.png" class="img-adptive" alt="Proteção de Dados Pessoais"></a>
-                </div>
-                <div class="description">
-                        <?php
-                        $page_marcocivil = get_page_by_title('Marco Civil da Internet');
-
-                        if ($page_marcocivil != null) {
-                            echo $page_marcocivil->post_content;
-                        }
-                        ?>
-                </div>
-            </div>
-            <div class="mt-sm text-center">
-                <a href="<?php echo site_url("/marcocivil"); ?>" class="btn btn-danger font-roboto">Participe da sistematização!</a>
+<div id="pagina-debates">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="red font-roboto">Debates públicos</h1>
             </div>
         </div>
     </div>
+    <header class="debates-intro">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3 class="red font-roboto">Participe, sua opinião é muito importante!</h3>
+                    <p class="mt-md">A realização de debates envolve cidadãos interessados nas discussões de projetos ou anteprojetos de lei, objetivando complementar, ou até mesmo substituir, formas tradicionais de elaboração, como por exemplo a formação de comissões de juristas.</p>
+                </div>
+                <div class="col-sm-5 image-header">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/img-debates-001.png" class="img-full" alt="Proteção de Dados Pessoais">
+                </div>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="red font-roboto">Debates em destaque</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="debate-item">
+                        <header class="anticorrupcao">
+                            <div class="text-center">
+                                <a href="<?php echo site_url("/anticorrupcao"); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/logos/logo-anticorrupcao-w.png" class="img-adptive mt-lg" alt="Medidas Anticorrupção"></a>
+                            </div>
+                        </header>
+                        <div class="description">
+                            <p>
+                                Depois do sucesso da plataforma digital que ajudou a construir o Marco Civil da Internet e debateu os Dados Abertos, agora é a vez do governo pedir ajuda à sociedade para encontrar novas medidas de combate à corrupção e impunidade.
+                            </p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p>
+                                        <a href="<?php echo site_url("/anticorrupcao"); ?>" class="btn btn-danger font-roboto">Confira como foi este debate!</a>
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="text-right mt-xs">
+                                        <span class="fontsize-sm text-orange"><i class="fa fa-exclamation-triangle"></i> Debate encerrado dia 8 de julho de 2015</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="red font-roboto">Debates recentes</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="debate-item">
+                        <header class="marco-civil">
+                            <div class="text-center">
+                                <a href="<?php echo site_url("/marco-civil"); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/logos/marcocivil-w.png" class="img-adptive" alt="Marco Civil da Internet"></a>
+                            </div>
+                        </header>
+                        <div class="description">
+                            <p>
+                                Essa regulamentação será feita de maneira colaborativa, utilizando uma plataforma   participativa, seguindo o padrão de debate público utilizado quando o Marco Civil ainda era um Anteprojeto de Lei.
+                            </p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p>
+                                        <a href="<?php echo site_url("/marco-civil"); ?>" class="btn btn-danger font-roboto">Confira como foi este debate!</a>
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="text-right mt-xs">
+                                        <span class="fontsize-sm text-orange"><i class="fa fa-exclamation-triangle"></i> Debate encerrado dia 8 de julho de 2015</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="debate-item">
+                        <header class="protecao">
+                            <div class="text-center">
+                                <a href="<?php echo site_url("/protecao"); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/logos/protecao-w.png" class="img-adptive" alt="Proteção de dados pessoais"></a>
+                            </div>
+                        </header>
+                        <div class="description">
+                            <p>
+                                O debate busca promover a participação da sociedade brasileira na elaboração do anteprojeto de lei para proteção de dados pessoais, por meio da formulação de comentários e sugestões sobre o texto proposto.
+                            </p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p>
+                                        <a href="<?php echo site_url("/marco-civil"); ?>" class="btn btn-danger font-roboto">Confira como foi este debate!</a>
+                                    </p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="text-right mt-xs">
+                                        <span class="fontsize-sm text-orange"><i class="fa fa-exclamation-triangle"></i> Debate encerrado dia 8 de julho de 2015</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="red font-roboto">Outros debates</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <section class="mt-md divider-bottom pb-md">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <a href="<?php echo site_url("https://participacao.mj.gov.br/pensandoodireito/siouv/"); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/outros/consultasiovu-230x175.jpg" class="img-adptive" alt="Sistema Público de Ouvidorias"></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <h4><strong class="red"><a href="<?php echo site_url("https://participacao.mj.gov.br/pensandoodireito/siouv/"); ?>" target="_blank">Sistema Público de Ouvidorias</a></strong></h4>
+                                <p>Reconhecendo a importância das ouvidorias públicas, que têm se destacado como instrumentos de defesa dos direitos fundamentais dos cidadãos</p>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="well well-sm">
+                                    <p>Iniciado em: 00/00/0000</p>
+                                    <p>Finalizado: 00/00/0000</p>
+                                    <p class="divider-top"><i class="fa fa-users"></i> 0000 participaram</p>
+                                    <p><i class="fa fa-comments"></i> comentários</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="mt-md divider-bottom pb-md">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <a href="<?php echo site_url("https://participacao.mj.gov.br/pensandoodireito/codcom/"); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/outros/codcom-230x175.png" class="img-adptive" alt="Código Comercial"></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <h4><strong class="red"><a href="<?php echo site_url("https://participacao.mj.gov.br/pensandoodireito/siouv/"); ?>" target="_blank">Código Comercial</a></strong></h4>
+                                <p>A proposta do projeto é caracterizar o Direito Empresarial e fortalecê-lo enquanto ciência autônoma, consolidando e atualizando uma série de disposições presentes em variados textos legais</p>
+                            </div>
+                             <div class="col-sm-4">
+                                <div class="well well-sm">
+                                    <p>Iniciado em: 00/00/0000</p>
+                                    <p>Finalizado: 00/00/0000</p>
+                                    <p class="divider-top"><i class="fa fa-users"></i> 0000 participaram</p>
+                                    <p><i class="fa fa-comments"></i> comentários</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="mt-md pb-md">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <a href="<?php echo site_url("https://participacao.mj.gov.br/pensandoodireito/codcom/"); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/debates/outros/cpc-230x175.png" class="img-adptive" alt="Código do Processo Civil"></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <h4><strong class="red"><a href="<?php echo site_url("https://participacao.mj.gov.br/pensandoodireito/cpc/"); ?>" target="_blank">Código do Processo Civil</a></strong></h4>
+                                <p>A proposta do projeto é caracterizar o Direito Empresarial e fortalecê-lo enquanto ciência autônoma, consolidando e atualizando uma série de disposições presentes em variados textos legais</p>
+                            </div>
+                             <div class="col-sm-4">
+                                <div class="well well-sm">
+                                    <p>Iniciado em: 00/00/0000</p>
+                                    <p>Finalizado: 00/00/0000</p>
+                                    <p class="divider-top"><i class="fa fa-users"></i> 0000 participaram</p>
+                                    <p><i class="fa fa-comments"></i> comentários</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
-
 <?php get_template_part('mini-tutorial'); ?>
-
 <?php get_footer(); ?>
