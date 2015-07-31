6d97e09e2f1ae15effd5b0c643bab15f03cbc23c
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-07-31 10:21:13 -0300

    criacão do popup pra compartilhamento social

--- part-social.php
+++ part-social.php
@@ -1,5 +1,5 @@
 <small>
-    <a href="#" onClick="event.preventDefault(); window.open('https://www.facebook.com/sharer/sharer.php?u=<?php echo get_post_permalink(); ?>','Facebook');" class="nounderline">
+    <a href="javascript:var socialw = window.open('https://www.facebook.com/sharer/sharer.php?u=<?php echo get_post_permalink(); ?>', 'socialw', 'width=570, height=350, location=no');" class="nounderline">
             <span class="fa-stack fa-lg">
               <i class="fa fa-square-o fa-stack-2x"></i>
               <i class="fa fa-facebook fa-stack-1x"></i>
@@ -7,7 +7,7 @@
     </a>
 </small>
 <small>
-    <a href="#" onClick="event.preventDefault(); window.open('https://twitter.com/share?hashtags=pensandoodireito&text=<?php echo the_title(); ?>&url=<?php echo get_post_permalink(); ?>','Tweet');" class="nounderline">
+    <a href="javascript:var socialw = window.open('https://twitter.com/share?hashtags=pensandoodireito&text=<?php echo the_title(); ?>&url=<?php echo get_post_permalink(); ?>', 'socialw', 'width=570, height=350, location=no');" class="nounderline">
             <span class="fa-stack fa-lg">
               <i class="fa fa-square-o fa-stack-2x"></i>
               <i class="fa fa-twitter fa-stack-1x"></i>
@@ -15,7 +15,7 @@
     </a>
 </small>
 <small>
-    <a href="#" onClick="event.preventDefault(); window.open('https://www.linkedin.com/shareArticle?mini=true&title=<?php echo urlencode(get_the_title()); ?>&url=<?php echo urlencode(get_post_permalink()); ?>&summary=<?php echo urlencode(get_the_excerpt()); ?>&source=http://participacao.mj.gov.br');" class="nounderline">
+    <a href="javascript:var socialw = window.open('https://www.linkedin.com/shareArticle?mini=true&title=<?php echo urlencode(get_the_title()); ?>&url=<?php echo urlencode(get_post_permalink()); ?>&summary=<?php echo urlencode(get_the_excerpt()); ?>&source=http://pensando.mj.gov.br', 'socialw', 'width=670, height=450, location=no');" class="nounderline">
             <span class="fa-stack fa-lg">
               <i class="fa fa-square-o fa-stack-2x"></i>
               <i class="fa fa-linkedin fa-stack-1x"></i>
